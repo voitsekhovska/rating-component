b5d17bc2b03681a4068c38f2b26953552d8225b2
ye.voitsekhovska <sunakojane6@gmail.com>
2025-07-23 01:05:26 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const form = document.querySelector(".form");
+const ratingInputs = document.querySelectorAll('.form input[type="radio"]');
+const ratingLabel = document.querySelectorAll(".form label");
+const ratingSection = document.querySelector(".rating-section");
+const thankYouSection = document.querySelector(".thank-you-section");
+const selectedSpan = document.querySelector(".selected-rating span");
+
+let currentRating;
+
+const ifSelected = (event) => {
+  ratingInputs.forEach((input) => {
+    input.classList.remove("selected");
+  });
+
+  const radio = event.target;
+
+  if (radio.checked) {
+    radio.parentElement.classList.add("selected");
+    currentRating = radio.value;
+  }
+};
+
+const updateForm = (event) => {
+  event.preventDefault();
+
+  if (currentRating) {
+    selectedSpan.textContent = currentRating;
+    ratingSection.classList.add("hidden");
+    thankYouSection.classList.remove("hidden");
+  } else {
+    alert("Option required.");
+  }
+};
+
+ratingInputs.forEach((radio) => radio.addEventListener("change", ifSelected));
+form.addEventListener("submit", updateForm);
